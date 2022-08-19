e1d3d14b6d3411635c8231bd1b3aeac7426647c8
fahadarpon <arponfahad@gmail.com>
2022-08-19 18:36:37 +0600

    9th commit

--- view/src/components/Login/Login.js
+++ view/src/components/Login/Login.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import './Login.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
+import './Login.css';
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -15,6 +14,8 @@ const Login = () => {
     ] = useSignInWithEmailAndPassword(auth);
 
     const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/';
 
     const handleEmailBlur = event => {
         setEmail(event.target.value);
@@ -25,7 +26,8 @@ const Login = () => {
     }
 
         if (user) {
-            navigate('/shop');
+            navigate(from, {replace: true});
+
         }
 
     const handleUserSignIn = event => {
