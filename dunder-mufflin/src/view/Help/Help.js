574c90a37538770153b32af606b03cd513449a9e
fahadarpon <arponfahad@gmail.com>
2022-08-21 01:18:35 +0600

    13th commit

--- dunder-mufflin/src/view/Help/Help.js
+++ dunder-mufflin/src/view/Help/Help.js
@@ -3,7 +3,88 @@ import './Help.css'
 const Help = () => {
     return (
         <div>
-            <h2>This is Help</h2>
+<img src='help2.png'></img>
+
+
+            <h3>Q. How does the site work? </h3>
+            <p>A. You can browse the site or use our search engine to find your desired products. You can then add them to your shopping bag and click on place order. You let us know your address, select a delivery time – and voila, you are done.  A Dunder Mufflin representative will then deliver your order right to your home or office. </p>
+
+
+
+            <h3>Q. What is the delevery cost? </h3>
+            <table>
+            <tr>
+    <td>Company</td>
+    <td>Orders below 400</td>
+    <td>Orders avobe 400</td>
+  </tr>
+  <tr>
+    <td>Dhaka</td>
+    <td>14</td>
+    <td>10</td>
+  </tr>
+
+  <tr>
+    <td>Khulna</td>
+    <td>14</td>
+    <td>10</td>
+  </tr>
+
+
+
+  <tr>
+    <td>Rajshahi</td>
+    <td>14</td>
+    <td>10</td>
+  </tr>
+
+
+
+  <tr>
+    <td>Borishal</td>
+    <td>14</td>
+    <td>10</td>
+  </tr>
+
+
+
+  <tr>
+    <td>Chattrogram</td>
+    <td>14</td>
+    <td>10</td>
+  </tr>
+
+
+
+  <tr>
+    <td>Gazipur</td>
+    <td>14</td>
+    <td>10</td>
+  </tr>
+
+</table>
+
+
+<h3>Q. How do I know when my order is here? </h3>
+            <p> A Dunder Mufflin representative will call you as soon as they are at your house to let you know about your delivery.  </p>
+
+
+
+            <h3>Q.  How do I pay? </h3>
+            <p> We accept cash on delivery and we also have Online Credit Card and Online bKash service. Don’t worry, our Dunder Mufflin representatives should always carry enough change.  </p>
+
+
+
+ <h3>Q.  What if the item is out of stock?  </h3>
+            <p> We hold our own inventory in our warehouses, so we rarely run out of stock. However, we will try our best to source what you need. If we cannot find it, we will SMS/call you and let you know what substitutes are available.   </p>
+
+
+
+
+
+
+
+
         </div>
     );
 };
