5a1ebafa86573afd16a5be90f5e095ba26768031
fahadarpon <arponfahad@gmail.com>
2022-08-21 01:32:20 +0600

    15th commit

--- dunder-mufflin/src/Controller/controller.js
+++ dunder-mufflin/src/Controller/controller.js
@@ -1,39 +1,30 @@
-const Product=async(req, res) =>{
-    console.log('query', req.query);
-    const page = parseInt(req.query.page);
-    const size = parseInt(req.query.size);
+const { Product, productCount, productKeys } = require("./controller");
 
-    const query = {};
-    const cursor = productCollection.find(query);
-    let products;
-    if(page || size){
-        // 0 --> skip: 0 get: 0-10(10): 
-        // 1 --> skip: 1*10 get: 11-20(10):
-        // 2 --> skip: 2*10 get: 21-30 (10):
-        // 3 --> skip: 3*10 get: 21-30 (10):
-        products = await cursor.skip(page*size).limit(size).toArray();
-    }
-    else{
-        products = await cursor.toArray();
-    }
-    
-    res.send(products);
-}
-const productCount= async(req, res) =>{
-    const count = await productCollection.estimatedDocumentCount();
-    res.send({count});
-}
-const productKeys=async(req, res) =>{
-    const keys = req.body;
-    const ids = keys.map(id => ObjectId(id));
-    const query = {_id: {$in: ids}}
-    const cursor = productCollection.find(query);
-    const products = await cursor.toArray();
-    console.log(keys);
-    res.send(products);
-}
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.btzpb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-  module.exports = {
-    Product,productCount,productKeys
-   
-  }
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const productCollection = client.db('emaJohn').collection('product');
+
+        app.get('/product',Product );
+
+        app.get('/productCount',productCount);
+
+        // use post to get products by ids
+        app.post('/productByKeys',productKeys )
+
+    }
+    finally{}
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) =>{
+    res.send('John is running and waiting for Ema')
+});
+
+app.listen(port, () =>{
+    console.log('John is running on  port', port);
+})
